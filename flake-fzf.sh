cba8f9a5994a74329e68f41bb7172fa15aa16436
Jean-François Roche <jfroche@pyxel.be>
2024-01-16 09:21:23 +0100

    Handle more flake output types
    Run formatter and app outputs
    Develop devShells

--- flake-fzf.sh
+++ flake-fzf.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
-
 if [[ ${2:-} == "--print" ]]; then
   MODE="print"
 else
@@ -31,16 +30,44 @@ selection=$(
 
 selectedPath="$(echo -n "$selection" | cut -d$'\t' -f1 | tr -d '\n')"
 selectedType="$(echo -n "$selection" | cut -d$'\t' -f2 | tr -d '\n')"
+
+case $selectedPath in
+  devShells.*)
+    selectedType="devShell"
+    ;;
+  formatter.*)
+    selectedType="formatter"
+    ;;
+  templates.*)
+    selectedType="template"
+    ;;
+esac
+
 case $selectedType in
   "nixos-configuration")
     installable="${flakePath}#$selectedPath.config.system.build.toplevel"
+    subcommand="build"
+    ;;
+  "app" | "formatter")
+    installable="${flakePath}#$selectedPath"
+    subcommand="run"
+    ;;
+  "devShell")
+    installable="${flakePath}#$selectedPath"
+    subcommand="develop"
+    ;;
+  "template")
+    installable="${flakePath}#$selectedPath"
+    subcommand="flake init -t"
     ;;
   *)
     installable="${flakePath}#$selectedPath"
+    subcommand="build"
     ;;
 esac
+
 if [[ $MODE == "print" ]]; then
-  echo "nix build -L $installable"
+  echo "nix $subcommand -L $installable"
 else
-  nix build -L "$installable"
+  nix "$subcommand" -L "$installable"
 fi
